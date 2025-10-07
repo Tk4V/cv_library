42377d73e5eaae1a894598181a6292ac78e332de
Vasyl Tkach <vasyl.tkach518@gmail.com>
2025-10-07 09:49:15 +0300

    Removed email_service import as it's replaced by SendGrid

--- celery_tasks/services/__init__.py
+++ celery_tasks/services/__init__.py
@@ -4,7 +4,6 @@ Services package for Celery tasks.
 from .base_service import BaseService
 from .analysis_service import AnalysisService
 from .pdf_service import PDFService
-from .email_service import EmailService
 from .translation_service import TranslationService
 from .sendgrid_service import SendGridService
 
@@ -12,7 +11,6 @@ __all__ = [
     'BaseService',
     'AnalysisService',
     'PDFService', 
-    'EmailService',
     'TranslationService',
     'SendGridService',
 ]
